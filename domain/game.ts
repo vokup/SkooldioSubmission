283c6a1e36f873c74480fa7fd7afe317ffbe0a33
vokup <thanapawee.film@gmail.com>
2020-11-26 00:07:28 +0700

    introduce game loop

--- domain/game.ts
+++
@@ -1,6 +0,0 @@
-export class Game {
-
-    async start(): Promise<void> {
-        console.log('Please put your bet');
-    }
-}
--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 import { Card } from "./domain/card";
+import { Game } from "./application/game";
 import { DeckDomainService } from "./domainservice/deck";
 import { SimpleRandomService } from "./infrastructure/random.service";
+import readline from 'readline';
+import { ConsoleGameIO } from "./infrastructure/gameio.console";
+
+
 
 async function main(): Promise<void> {
     const randomService = new SimpleRandomService();
     const deckDomainService = new DeckDomainService(randomService);
 
     const deck = deckDomainService.createDeck();
-    // console.log(deck);
     deckDomainService.shuffleDeck(deck);
-    // console.log(deck);
 
-    console.log(deckDomainService.dealFromDeck(deck));
-    console.log(deckDomainService.dealFromDeck(deck));
-    console.log(deckDomainService.dealFromDeck(deck));
-    console.log(deckDomainService.dealFromDeck(deck));
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    var game = new Game(deckDomainService, new ConsoleGameIO(rl));
+    await game.start();
+    rl.close();
 }
 
 main();
